37082ff29b5291390f2ed7db59de6a697ab2bf04
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-04-03 08:47:38 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,101 @@
+# REQUIRED CONFIG
+BOT_TOKEN = "6278255552:AAGsi6HpVwhh48XcbdskaD6NK-tQVgYQrzM"
+OWNER_ID = "5179011789"
+TELEGRAM_API = "25918874"
+TELEGRAM_HASH = "87c7c525932cf3d753bea33786ad71ee"
+
+# OPTIONAL CONFIG
+USER_SESSION_STRING = ""
+DATABASE_URL = "mongodb+srv://Testx:Testx@cluster0.ttnrz0x.mongodb.net/?retryWrites=true&w=majority"
+DOWNLOAD_DIR = "/usr/src/app/downloads/"
+CMD_SUFFIX = ""
+AUTHORIZED_CHATS = "-1001837453459"
+SUDO_USERS = ""
+STATUS_LIMIT = "3"
+DEFAULT_UPLOAD = "gd"
+STATUS_UPDATE_INTERVAL = "10"
+AUTO_DELETE_MESSAGE_DURATION = "30"
+UPTOBOX_TOKEN = ""
+EXTENSION_FILTER = ""
+INCOMPLETE_TASK_NOTIFIER = "True"
+YT_DLP_QUALITY = ""
+# GDrirve Tools
+GDRIVE_ID = "12XXbsIVcvPdKmNonPt66ozuwIk7hMolH"
+IS_TEAM_DRIVE = "True"
+USE_SERVICE_ACCOUNTS = "False"
+STOP_DUPLICATE = "True"
+INDEX_URL = "https://rulfmltb.auternortesanousc-q-4-142.workers.dev/0:/"
+# Rclone
+RCLONE_PATH = ""
+RCLONE_FLAGS = ""
+RCLONE_SERVE_URL = ""
+RCLONE_SERVE_PORT = ""
+RCLONE_SERVE_USER = ""
+RCLONE_SERVE_PASS = ""
+LEECH_SPLIT_SIZE = ""
+AS_DOCUMENT = "False"
+EQUAL_SPLITS = "False"
+MEDIA_GROUP = "False"
+LEECH_FILENAME_PERFIX = ""
+DUMP_CHAT = "-1001590960992"
+# qBittorrent/Aria2c
+TORRENT_TIMEOUT = ""
+BASE_URL = "https://jmdkhrulfbot.herokuapp.com"
+BASE_URL_PORT = "80"
+WEB_PINCODE = "False"
+#Queueing system
+QUEUE_ALL = ""
+QUEUE_DOWNLOAD = ""
+QUEUE_UPLOAD = ""
+# RSS
+RSS_DELAY = "900"
+RSS_CHAT_ID = ""
+# Mega
+MEGA_API_KEY = ""
+MEGA_EMAIL_ID = ""
+MEGA_PASSWORD = ""
+# Buttons
+VIEW_LINK = "False"
+# Torrent Search
+SEARCH_API_LINK = ""
+SEARCH_LIMIT = "0"
+SEARCH_PLUGINS = '["https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/rarbg.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/piratebay.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/legittorrents.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/limetorrents.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentscsv.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torlock.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/eztv.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentproject.py",
+                   "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/solidtorrents.py",
+                   "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/kickass_torrent.py",
+                   "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/yts_am.py",
+                   "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/linuxtracker.py",
+                   "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/nyaasi.py",
+                   "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/ettv.py",
+                   "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/glotorrents.py",
+                   "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/thepiratebay.py",
+                   "https://raw.githubusercontent.com/nindogo/qbtSearchScripts/master/magnetdl.py",
+                   "https://raw.githubusercontent.com/msagca/qbittorrent_plugins/main/uniondht.py",
+                   "https://raw.githubusercontent.com/khensolomon/leyts/master/yts.py"]'
+
+#Limits
+STORAGE_THRESHOLD = ""
+TORRENT_LIMIT = ""
+DIRECT_LIMIT = ""
+YTDLP_LIMIT = ""
+GDRIVE_LIMIT = ""
+MEGA_LIMIT = ""
+LEECH_LIMIT = ""
+CLONE_LIMIT = ""
+FSUB_IDS = "-1001579977875"
+USER_MAX_TASKS = "2"
+REQUEST_LIMITS = "True"
+ENABLE_MESSAGE_FILTER = "True"
+STOP_DUPLICATE_TASKS = "True"
+DISABLE_DRIVE_LINK = "False"
+SET_COMMANDS = "True"
+DISABLE_LEECH = "False"
+DM_MODE = "all"
+DELETE_LINKS = "True"
+LOG_CHAT = "-1001816495885"
